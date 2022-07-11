2ba1849f67045ac3ee7855bc10af92c025982a7f
kkdd8878 <kkdd4545@naver.com>
2022-07-11 11:39:51 +0900

    프로젝트 생성

---
+++ smiledoor/src/test/java/com/example/smiledoor/SmiledoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smiledoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmiledoorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
